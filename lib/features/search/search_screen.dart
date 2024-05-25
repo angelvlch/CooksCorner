53e93a916028876bc1bf86042aac5718baad893f
Angelina <angelforworkpay@gmail.com>
2024-05-25 14:04:13 +0300

    add tab bar

--- lib/features/search/search_screen.dart
+++ lib/features/search/search_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cooks_corner/core/constants/app_colors.dart';
 import 'package:cooks_corner/core/constants/app_font.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -11,19 +12,56 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  final List<Tab> tabs = [
-    const Tab(text: 'Chefs'),
-    const Tab(text: 'Recipes'),
-  ];
-
   @override
   Widget build(BuildContext context) {
+    List<Tab> tabs = [
+      Tab(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.sizeOf(context).width * 44 / 392,
+              vertical: 4),
+          child: const Text('Chefs'),
+        ),
+      ),
+      Tab(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.sizeOf(context).width * 44 / 392,
+              vertical: 4),
+          child: const Text('Recipes', overflow: TextOverflow.ellipsis),
+        ),
+      ),
+    ];
     return DefaultTabController(
       length: tabs.length,
       child: Scaffold(
         appBar: AppBar(
-          bottom: TabBar(
-            tabs: tabs,
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(100),
+            child: Container(
+              height: 52,
+              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(100),
+                ),
+                color: AppColors.secondary,
+              ),
+              child: TabBar(
+                indicatorWeight: 0,
+                overlayColor:
+                    const MaterialStatePropertyAll(Colors.transparent),
+                labelColor: AppColors.background,
+                labelStyle: AppFonts.s14w500,
+                dividerColor: Colors.transparent,
+                indicator: const BoxDecoration(
+                  color: AppColors.orange,
+                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                ),
+                tabs: tabs,
+              ),
+            ),
           ),
           centerTitle: true,
           title: const Text(
@@ -32,16 +70,9 @@ class _SearchScreenState extends State<SearchScreen> {
             textAlign: TextAlign.center,
           ),
         ),
-        body: Column(
+        body: const Column(
           children: [
             SizedBox(height: 24),
-            Container(
-              height: 20,
-              width: 100,
-              decoration: BoxDecoration(
-                  color: AppColors.secondary,
-                  borderRadius: BorderRadius.circular(12)),
-            ),
             Expanded(
               child: TabBarView(children: [
                 Icon(Icons.flight, size: 350),
