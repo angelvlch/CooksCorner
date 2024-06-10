39861f238eaff09c409f841ea9d5bf8522099eb4
Angelina <angelforworkpay@gmail.com>
2024-06-10 18:27:44 +0300

    add readme file

--- lib/features/search/data/data_source/impl/search_data_source_impl.dart
+++ lib/features/search/data/data_source/impl/search_data_source_impl.dart
@@ -1,9 +1,10 @@
 import 'package:cooks_corner/features/search/data/data_source/search_data_source.dart';
 
-class SearchDataSourceImpl implements SearchDataSource {
+/* class SearchDataSourceImpl implements SearchDataSource {
   @override
   Future<List<SearchChefsModel>> getSearchChefs() {
     // TODO: implement getSearchChefs
     throw UnimplementedError();
   }
 }
+ */
